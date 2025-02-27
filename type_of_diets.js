67c935b0e33e27118e085c0bc4c16e03f3faf4a9
Amiyl <136658858+Amiyl@users.noreply.github.com>
2025-02-27 08:14:57 -0500

    Add files via upload

---
+++ type_of_diets.js
@@ -0,0 +1,25 @@
+if(Response=="keto"){
+    display = 'Your diet is Keto'
+}
+else if(Response=="low-carbs"){
+    display = 'Your diet os low-Carb'
+
+}
+else if(Response=="Vegan"){
+    display = 'Your diet os Vegan'
+
+}
+else if(Response=="Balanced-diet"){
+    display = 'Your diet os Balanced-diet'
+
+}
+else if(Response=="Intermentting-fasting"){
+    display = 'Your diet os Intermentting-fasting'
+
+}
+
+
+
+
+
+
