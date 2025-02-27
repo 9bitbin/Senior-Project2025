a593f261103407a18930080518e663eddccb83ac
sabrinaachille <sabrinaachille1@gmail.com>
2025-02-26 19:26:16 -0500

    Update Index

---
+++ SeniorProject25/index_script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const signupAbBtn = document.querySelector("nav .button:first-of-type"); 
+
+    if (signupAbBtn) {
+        signupAbBtn.addEventListener("click", (event) => {
+            event.preventDefault(); 
+            localStorage.setItem("signupClicked", "true"); 
+            window.location.href = "login.html"; 
+        });
+    }
+});
