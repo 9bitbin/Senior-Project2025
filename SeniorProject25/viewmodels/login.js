67e0ada65fb827cae4ae9f20abbf478fad5fd966
9bitbin <104921088+9bitbin@users.noreply.github.com>
2025-03-05 22:49:30 -0500

    update

--- SeniorProject25/viewmodels/login.js
+++ SeniorProject25/viewmodels/login.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -47,3 +47,11 @@ signup.addEventListener("click",function(event){
   });
 })
 
+//reset 
+const reset = document.getElementById("reset");
+reset.addEventListener("click",function(event){
+  event.preventDefault()
+
+  const email = document.getElementById("email").value;
+
+})
